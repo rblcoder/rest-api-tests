bbb94e501aa670972967fd97cb9447e2bb8eaa88
Rupa Lahiri <35212670+rblcoder@users.noreply.github.com>
2024-11-18 06:03:44 +0000

    Instantiate Settings in config file

--- src/database.py
+++ src/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from src.config import Settings
+from src.config import Config
 
 # DATABASE_URL = "sqlite:///./test.db"
-DATABASE_URL = Settings().DATABASE_URL
+DATABASE_URL = Config.DATABASE_URL
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
